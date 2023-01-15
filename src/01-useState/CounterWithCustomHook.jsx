efb1e04c74e6c215172b555360c21aef644bf0e1
CharlyLuna <cluna10@ucol.mx>
2023-01-15 15:00:26 -0600

    Add base files and counter components

---
+++ src/01-useState/CounterWithCustomHook.jsx
@@ -0,0 +1,11 @@
+export const CounterWithCustomHook = () => {
+  return (
+    <>
+      <h1>Counter With Hook:</h1>
+
+      <button className='btn btn-primary'>+1</button>
+      <button className='btn btn-primary'>Reset</button>
+      <button className='btn btn-primary'>-1</button>
+    </>
+  );
+};
