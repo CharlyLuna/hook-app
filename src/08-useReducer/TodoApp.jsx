0ea2c239738a8f8ea7bbc6d4f0a43943790037a9
CharlyLuna <cluna10@ucol.mx>
2023-01-24 19:08:23 -0600

    Separate logic from TodoApp component

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -1,75 +1,37 @@
-import { useEffect, useReducer } from "react";
-import { TodoAdd, TodoList, todoReducer } from "./";
-
-const initialState = [
-  // {
-  //   id: new Date().getTime(),
-  //   description: "See Fernando God tutorials",
-  //   done: false,
-  // },
-  // {
-  //   id: new Date().getTime() * 4,
-  //   description: "Coding",
-  //   done: false,
-  // },
-];
-
-const init = () => {
-  return JSON.parse(localStorage.getItem("todos")) ?? [];
-};
+import { useTodo } from "../hooks/";
+import { TodoAdd, TodoList } from "./";
 
 export const TodoApp = () => {
-  const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+	const {
+		todos,
+		todosCount,
+		pendingTodosCount,
+		handleDeleteTodo,
+		handleToggleTodo,
+		handleNewTodo,
+	} = useTodo();
 
-  useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos));
-  }, [todos]);
+	return (
+		<>
+			<h1>
+				Todo App: {todosCount},<small>pendientes: {pendingTodosCount}</small>
+			</h1>
+			<hr />
 
-  const handleNewTodo = (todo) => {
-    const action = {
-      type: "Add Todo",
-      payload: todo,
-    };
-    dispatch(action);
-  };
-
-  const handleDeleteTodo = (id) => {
-    dispatch({
-      type: "Remove Todo",
-      payload: id,
-    });
-  };
-
-  const handleToggleTodo = (id) => {
-    dispatch({
-      type: "Toggle Todo",
-      payload: id,
-    });
-  };
-
-  return (
-    <>
-      <h1>
-        Todo App (10) <small>pendientes: 2</small>
-      </h1>
-      <hr />
-
-      <div className='row'>
-        <div className='col-7'>
-          {/* Todo list */}
-          <TodoList
-            todos={todos}
-            onDeleteTodo={handleDeleteTodo}
-            onToggleTodo={handleToggleTodo}
-          />
-        </div>
-        <div className='col-5'>
-          <h4>Add TODO</h4>
-          <hr />
-          {/* TodoAdd */}
-          <TodoAdd handleNewTodo={handleNewTodo} />
-        </div>
-      </div>
-    </>
-  );
+			<div className='row'>
+				<div className='col-7'>
+					<TodoList
+						todos={todos}
+						onDeleteTodo={handleDeleteTodo}
+						onToggleTodo={handleToggleTodo}
+					/>
+				</div>
+				<div className='col-5'>
+					<h4>Add TODO</h4>
+					<hr />
+					<TodoAdd handleNewTodo={handleNewTodo} />
+				</div>
+			</div>
+		</>
+	);
 };
